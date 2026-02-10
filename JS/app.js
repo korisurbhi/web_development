9ed4fbb9ca36a9d875c1603399cf533311081de8
korisurbhi <surbhikori11096@gmail.com>
2026-02-10 20:22:40 +0530

    js

---
+++ JS/app.js
@@ -0,0 +1,28 @@
+console.log("Hello, World!");
+console.log(console)
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+console.info("This is an informational message.");
+console.count("Counter");
+console.table({name: "John", age: 30, city: "New York"});
+console.assert(2 + 2 === 4, "Math is broken!");
+
+// Variables
+let myAge = 21
+const myName = "Surbhi"
+console.log("My age is: " + myAge);
+console.log("My name is: " + myName);
+console.log("type of myAge: " + typeof myAge);
+
+// data types
+myAge = "hello"
+console.log("type of myAge: " + typeof myAge);
+
+// there are 7 data types in JavaScript
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. Object
--- app.js
+++
@@ -1,2 +0,0 @@
-console.log("hello world");
-// wd-8 , 47.10
