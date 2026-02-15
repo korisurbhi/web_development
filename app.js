412c9fb27f361005603ef98a138ec424096a96fc
korisurbhi <surbhikori11096@gmail.com>
2026-02-15 17:28:35 +0530

    db

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-
 const port = 5000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
 
 app.get('/', (req, res)=>{
-    console.log("hello js");
+    // console.log("hello js");
+    res.render('index');
 });
 
+app.post('/save', (req, res)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+
 app.listen(port, ()=>{console.log(`Server is running on port : ${port}`)})
+
