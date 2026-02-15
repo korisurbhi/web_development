5c8e78f1d39b0d5ebb794a3a476e523e6d4ef145
korisurbhi <surbhikori11096@gmail.com>
2026-02-15 22:04:51 +0530

    db

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const student = require('./module/Student');
+const mongoose  = require('mongoose');
 const app = express();
 
 const port = 5000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-
+mongoose.connect('mongodb://localhost:27017/studentDB')
+    .then(() => console.log('DB connected successfully'))
+    .catch((err) => console.error('DB not connected:'+ err));  
 
 app.get('/', (req, res)=>{
-    // console.log("hello js");
     res.render('index');
 });
 
-app.post('/save', (req, res)=>{
-    console.log(req.body);
+app.post('/save', async (req, res)=>{
+    
+    const { rollNo, name, degree, city } = req.body;
+    const data = new student({rollNo, name, degree, city
+    });
+    await data.save();
+
     res.redirect('/')
 })
 
---
+++ module/Student.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const studentSchema = mongoose.Schema({
+    rollNo: String,
+    name: String,
+    degree: String,
+    city: String
+})
+
+module.exports = mongoose.model('Student', studentSchema);
